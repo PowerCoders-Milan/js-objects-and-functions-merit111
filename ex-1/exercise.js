31156919ee8fb858623c97169eac735be5f6bd56
Your Name Here <ebrimaj604@gmail.com>
2021-02-26 12:14:00 +0100

    ex-1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,13 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function  user(){
+    var noun = prompt('Can you please input a noun');
+    var verb = prompt('Can you please input a verb');
+    var adverb = prompt('Can you please input a adverb');
+    var adjective = prompt('Can you please input a adjective');
+    var user = ("I adore" + verb+ "basketball" +noun+ 'I live in Bayern');
+    windows.alert(user);
+}
+
+user();
