e6d70ff96de343cac93266b4abe6dcc2b89f1a74
Your Name Here <ebrimaj604@gmail.com>
2021-02-26 17:42:53 +0100

    ex-4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,3 +1,20 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+//Improve your age calculator by using a function
+
+//Rebuild the age calculator by using new Date()
+
+//Then use that inside a function
+
+//Call that function several times with different dates
+
+function agecalculator() {
+    birthYear = prompt("Pleaase put your Birth year");
+    currentYear = new Date();
+    age = currentYear.getFullYear()-birthYear;
+    return "your age is " + age;
+}
+
+window.alert(agecalculator());
+window.alert(agecalculator());
