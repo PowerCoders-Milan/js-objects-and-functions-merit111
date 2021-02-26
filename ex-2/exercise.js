5fbb9bed1d1b2baf988e1d9c691419e81e5b76fc
Your Name Here <ebrimaj604@gmail.com>
2021-02-26 15:08:47 +0100

    ex-2 done

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,26 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+
+var numberOfChildren = prompt(Please enter number of children);
+var partnerName = prompt(Please enter partner name);
+var location = prompt(Please enter your location);
+var jobTitle = prompt(Please enter your job title);
+
+var numberOfChildren = "4"
+var partnerName = "Jasmine"
+var location = "Augsburg"
+var jobTitle = "Manager"
+
+function fortuneTeller(numberOfChildren, partnerName, Location, jobTitle){
+   this.numberOfChildren = numberOfChildren
+   this.partnerName = partnerName
+   this.Location = Location
+   this.jobTitle = jobTitle
+   alert('You will be a '+jobTitle+' in '+Location+', and married to '+partnerName+' with '+numberOfChildren+' kids.')
+}
+
+var merit = new fortuneTeller(numberOfChildren, partnerName, location, jobTitle);
+
+merit.fortuneTeller();
